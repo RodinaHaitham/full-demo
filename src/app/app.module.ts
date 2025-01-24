030b823b1f1711bdef41208d6ee7892576a02270
RodinaHaitham <147608393+RodinaHaitham@users.noreply.github.com>
2025-01-25 00:22:23 +0200

    444

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Lc1/header/header.component';
+import { ProductsComponent } from './Lc1/products/products.component';
+import { FooterComponent } from './Lc1/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
